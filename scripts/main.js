1774c3fcb716c73b314efc479ecb824ab0182768
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-09-01 15:40:21 +0300

    Main script: Created support for Tranzilla donation page.

--- scripts/main.js
+++ scripts/main.js
@@ -13,8 +13,12 @@ function PostCards() {
 	var self = this;
 
 	self.container = null;
-	self.forms = null;
+	self.form_containers = null;
 	self.button = null;
+	self.checkbox = null;
+
+	self.iframe = null;
+	self.background = null;
 
 	/**
 	 * Complete object initialization.
@@ -22,11 +26,85 @@ function PostCards() {
 	self._init = function() {
 		self.container = $('div#postcard_container');
 		self.button = $('div.postcard_bottom button[type=button]');
-		self.forms = self.container.children('div');
+		self.checkbox = $('div.postcard_bottom input[type=checkbox]');
+		self.form_containers = self.container.children('div');
+		self.forms = self.container.find('form');
 
-		self.forms.click(self._handleFormClick);
+		// connect events
+		self.form_containers.click(self._handleFormClick);
 		self.button.click(self._handleSubmit);
+		self.forms.on('dialog-show', self._handleDialogShow);
 	}
+
+	/**
+	 * Show tranzilla donation page.
+	 */
+	self.showTranzilla = function() {
+		var form = $(this);
+		var data = {};
+		var url = 'https://direct.tranzila.com/<account>/iframe.php?';
+
+		if (self.iframe == null) {
+			self.iframe = $('<iframe>');
+			self.background = $('<div>');
+
+			// configure background
+			self.background
+				.addClass('iframe-background')
+				.click(self._handleBackgroundClick)
+				.appendTo($('body'));
+
+			// configure iframe
+			self.iframe
+				.attr('id', 'tranzila')
+				.attr('frameborder', 0)
+				.appendTo($('body'));
+		}
+
+		// prepare params for tranzilla
+		var params = {
+			lang: 'he',
+			sum: 100,
+			cred_type: 8,
+			currency: 1
+		};
+
+		self.iframe.attr('src', url + $.param(params));
+
+		// show iframe
+		setTimeout(function() {
+			self.background.addClass('visible');
+			self.iframe.addClass('visible');
+		}, 50);
+	};
+
+	/**
+	 * Handle clicking outside of I frame.
+	 */
+	self._handleBackgroundClick = function(event) {
+		event.preventDefault();
+
+		// hide elements
+		self.iframe.removeClass('visible');
+		self.background.removeClass('visible');
+	};
+
+	/**
+	 * Handle successful submission.
+	 *
+	 * @param object event
+	 * @return boolean
+	 */
+	self._handleDialogShow = function(event) {
+		var result = true;  // allow showing dialog by default
+
+		if (self.checkbox.is(':checked')) {
+			result = false;  // we are showing tranzilla, no need for thank you
+			self.showTranzilla();
+		}
+
+		return result;
+	};
 
 	/**
 	 * Handle clicking on form.
@@ -37,7 +115,7 @@ function PostCards() {
 		var form_container = $(this);
 
 		// remove active class
-		var affected_forms = self.forms.not(form_container);
+		var affected_forms = self.form_containers.not(form_container);
 
 		affected_forms.removeClass('active left right');
 		affected_forms.eq(0).addClass('left');
@@ -56,7 +134,7 @@ function PostCards() {
 		// prevent default behavior
 		event.preventDefault();
 
-		var container = self.forms.filter('.active');
+		var container = self.form_containers.filter('.active');
 		var current_form = container.find('form');
 		var field = current_form.find('input[name=version]');
 
