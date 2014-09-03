501809cfd356c1dfb174ec23404d4acdc8bbcce8
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-09-03 13:22:31 +0300

    Initial import of pkatan module.

---
+++ modules/pkatan/pkatan.php
@@ -0,0 +1,67 @@
+<?php
+
+use Core\Module;
+
+require_once('units/mailer.php');
+
+
+class pkatan extends Module {
+	private static $_instance;
+	private $mailer;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		parent::__construct(__FILE__);
+
+		if (class_exists('contact_form')) {
+			$contact_form = contact_form::getInstance();
+			$this->mailer = new PostcardMailer();
+
+			$contact_form->registerMailer('postcard', $this->mailer);
+		}
+	}
+
+	/**
+	 * Public function that creates a single instance
+	 */
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+			self::$_instance = new self();
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Transfers control to module functions
+	 *
+	 * @param array $params
+	 * @param array $children
+	 */
+	public function transferControl($params = array(), $children = array()) {
+		if (isset($params['action']))
+			switch ($params['action']) {
+				case 'test':
+					$this->mailer->set_variables(array(
+						'version'	=> 'holiday',
+						'name'		=> 'Mladen Mijatov',
+						'blessing'	=> 'לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קולהע צופעט למרקוח איבן איף, ברומץ כלרשט מיחוצים. קלאצי קולהע צופעט למרקוח איבן איף, ברומץ כלרשט מיחוצים. קלאצי סחטיר בלובק. תצטנפל בלינדו למרקל אס לכימפו, דול, צוט ומעיוט - לפתיעם ברשג - ולתיעם גדדיש. קוויז דומור ליאמום בלינך רוגצה.'
+					));
+					$this->mailer->send();
+					break;
+			}
+	}
+
+	/**
+	 * Event triggered upon module initialization
+	 */
+	public function onInit() {
+	}
+
+	/**
+	 * Event triggered upon module deinitialization
+	 */
+	public function onDisable() {
+	}
+}
